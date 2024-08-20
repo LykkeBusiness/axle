4a817a2af9822f1693f223b7acb80be0a2205556
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2024-08-20 13:59:37 +0200

    LT-5671: rename projects

--- src/Tests/Axle.Acceptance.Tests/Support/AxleCollection.cs
+++ src/Tests/Axle.Acceptance.Tests/Support/AxleCollection.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-namespace Axle.Tests.Acceptance.Support
+namespace Axle.Acceptance.Tests.Support
 {
     using Xunit;
 
